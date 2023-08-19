823d03a0c65cff56e231b91f1083810a043f34fe
Akshay200228 <akshayrs096@gmail.com>
2023-08-19 13:04:36 +0530

    Done User Detail form and API fetching

--- src/components/FirstPage/FirstPage.tsx
+++ src/components/FirstPage/FirstPage.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Button, TextField, Typography, Container } from '@mui/material';
 
 interface FirstPageProps {
   onSubmit: (name: string, phoneNumber: string, email: string) => void;
@@ -12,35 +13,75 @@ const FirstPage: React.FC<FirstPageProps> = ({ onSubmit }) => {
   const [email, setEmail] = useState('');
 
   const handleSubmit = () => {
-    if (name && phoneNumber && email) {
+    if (name.trim() === '' || phoneNumber.trim() === '' || email.trim() === '') {
+      alert('Please fill in all the details.');
+    } else if (!isValidPhoneNumber(phoneNumber)) {
+      alert('Please enter a valid phone number (10 digits).');
+    } else if (!isValidEmail(email)) {
+      alert('Please enter a valid email address.');
+    } else {
       onSubmit(name, phoneNumber, email);
       navigate('/second');
-    } else {
-      alert('Please enter all details');
     }
   };
 
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      handleSubmit();
+    }
+  };
+
+  const isValidPhoneNumber = (phoneNumber: string) => {
+    // Basic phone number validation: Exactly 10 digits
+    const phoneRegex = /^\d{10}$/;
+    return phoneRegex.test(phoneNumber);
+  };
+
+  const isValidEmail = (email: string) => {
+    // Basic email validation
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
   return (
-    <div>
-      <h2>First Page</h2>
+    <Container maxWidth="sm">
+      <Typography variant="h2">First Page</Typography>
       <form>
-        <div>
-          <label>Name:</label>
-          <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-        </div>
-        <div>
-          <label>Phone Number:</label>
-          <input type="text" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
-        </div>
-        <div>
-          <label>Email:</label>
-          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-        </div>
-        <button type="button" onClick={handleSubmit}>
+        <TextField
+          label="Name"
+          variant="outlined"
+          fullWidth
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          margin="normal"
+          onKeyPress={handleKeyPress} // Handle "Enter" key press
+        />
+        <TextField
+          label="Phone Number"
+          variant="outlined"
+          fullWidth
+          type="tel"
+          inputProps={{ pattern: '[0-9]{10}' }}
+          value={phoneNumber}
+          onChange={(e) => setPhoneNumber(e.target.value)}
+          margin="normal"
+          onKeyPress={handleKeyPress} // Handle "Enter" key press
+        />
+        <TextField
+          label="Email"
+          variant="outlined"
+          fullWidth
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          margin="normal"
+          onKeyPress={handleKeyPress} // Handle "Enter" key press
+        />
+        <Button variant="contained" color="primary" onClick={handleSubmit}>
           Submit
-        </button>
+        </Button>
       </form>
-    </div>
+    </Container>
   );
 };
 
--- src/components/SecondPage/SecondPage.tsx
+++ src/components/SecondPage/SecondPage.tsx
@@ -1,21 +1,51 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+// Define the model/interface for a Post
+interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
 
 const SecondPage: React.FC = () => {
   const navigate = useNavigate();
   const userDetails = localStorage.getItem('userDetails');
+  const [posts, setPosts] = useState<Post[]>([]);
 
   useEffect(() => {
     if (!userDetails) {
       alert('Please provide your details before accessing this page');
       navigate('/');
+      return; // Exit early if userDetails are missing
     }
+
+    async function fetchPosts() {
+      try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+        setPosts(response.data);
+      } catch (error) {
+        console.error('Error fetching posts:', error);
+      }
+    }
+
+    fetchPosts();
   }, [navigate, userDetails]);
 
   return (
     <div>
       <h2>Second Page</h2>
       <p>Welcome to the second page!</p>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };

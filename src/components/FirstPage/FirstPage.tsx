de72db957fa48608ba6c37257311dff68f815a67
Akshay200228 <akshayrs096@gmail.com>
2023-08-19 13:39:59 +0530

    Done error handling and Alerts

--- src/components/FirstPage/FirstPage.tsx
+++ src/components/FirstPage/FirstPage.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Button, TextField, Typography, Container } from '@mui/material';
+import { Button, TextField, Typography, Container, Alert, AlertTitle, Snackbar } from '@mui/material';
 
 interface FirstPageProps {
   onSubmit: (name: string, phoneNumber: string, email: string) => void;
@@ -8,20 +8,40 @@ interface FirstPageProps {
 
 const FirstPage: React.FC<FirstPageProps> = ({ onSubmit }) => {
   const navigate = useNavigate();
-  const [name, setName] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
-  const [email, setEmail] = useState('');
+  const [formData, setFormData] = useState({
+    name: '',
+    phoneNumber: '',
+    email: '',
+  });
+  const [showError, setShowError] = useState(false);
+  const [showPhoneError, setShowPhoneError] = useState(false);
+  const [showEmailError, setShowEmailError] = useState(false);
+
+  const handleChange = (field: keyof typeof formData, value: string) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      [field]: value,
+    }));
+  };
 
   const handleSubmit = () => {
+    const { name, phoneNumber, email } = formData;
+    let hasError = false;
+
     if (name.trim() === '' || phoneNumber.trim() === '' || email.trim() === '') {
-      alert('Please fill in all the details.');
+      setShowError(true);
+      hasError = true;
     } else if (!isValidPhoneNumber(phoneNumber)) {
-      alert('Please enter a valid phone number (10 digits).');
+      setShowPhoneError(true);
+      hasError = true;
     } else if (!isValidEmail(email)) {
-      alert('Please enter a valid email address.');
-    } else {
+      setShowEmailError(true);
+      hasError = true;
+    }
+
+    if (!hasError) {
       onSubmit(name, phoneNumber, email);
-      navigate('/second');
+      navigate('/second?success=true'); // Pass the success parameter
     }
   };
 
@@ -32,56 +52,77 @@ const FirstPage: React.FC<FirstPageProps> = ({ onSubmit }) => {
   };
 
   const isValidPhoneNumber = (phoneNumber: string) => {
-    // Basic phone number validation: Exactly 10 digits
     const phoneRegex = /^\d{10}$/;
     return phoneRegex.test(phoneNumber);
   };
 
   const isValidEmail = (email: string) => {
-    // Basic email validation
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
   };
 
   return (
-    <Container maxWidth="sm">
-      <Typography variant="h2">First Page</Typography>
-      <form>
-        <TextField
-          label="Name"
-          variant="outlined"
-          fullWidth
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          margin="normal"
-          onKeyPress={handleKeyPress} // Handle "Enter" key press
-        />
-        <TextField
-          label="Phone Number"
-          variant="outlined"
-          fullWidth
-          type="tel"
-          inputProps={{ pattern: '[0-9]{10}' }}
-          value={phoneNumber}
-          onChange={(e) => setPhoneNumber(e.target.value)}
-          margin="normal"
-          onKeyPress={handleKeyPress} // Handle "Enter" key press
-        />
-        <TextField
-          label="Email"
-          variant="outlined"
-          fullWidth
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          margin="normal"
-          onKeyPress={handleKeyPress} // Handle "Enter" key press
-        />
-        <Button variant="contained" color="primary" onClick={handleSubmit}>
-          Submit
-        </Button>
-      </form>
-    </Container>
+    <div className="first-page-container">
+      <div className="bg-image"></div>
+      <Container maxWidth="sm">
+        <Typography variant="h2">First Page</Typography>
+        <form>
+          <TextField
+            label="Name"
+            variant="outlined"
+            fullWidth
+            value={formData.name}
+            onChange={(e) => handleChange('name', e.target.value)}
+            margin="normal"
+            onKeyPress={handleKeyPress}
+          />
+          <TextField
+            label="Phone Number"
+            variant="outlined"
+            fullWidth
+            type="tel"
+            inputProps={{ pattern: '[0-9]{10}' }}
+            value={formData.phoneNumber}
+            onChange={(e) => handleChange('phoneNumber', e.target.value)}
+            margin="normal"
+            onKeyPress={handleKeyPress}
+          />
+          <TextField
+            label="Email"
+            variant="outlined"
+            fullWidth
+            type="email"
+            value={formData.email}
+            onChange={(e) => handleChange('email', e.target.value)}
+            margin="normal"
+            onKeyPress={handleKeyPress}
+          />
+          <Button variant="contained" color="primary" onClick={handleSubmit}>
+            Submit
+          </Button>
+          <Snackbar
+            open={showError || showPhoneError || showEmailError}
+            autoHideDuration={3000}
+            onClose={() => {
+              setShowError(false);
+              setShowPhoneError(false);
+              setShowEmailError(false);
+            }}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+          >
+            <Alert severity="error">
+              <AlertTitle>Error</AlertTitle>
+              {showError && 'Please fill in all the details.'}
+              {showPhoneError && 'Please enter a valid phone number (10 digits).'}
+              {showEmailError && 'Please enter a valid email address.'}
+            </Alert>
+          </Snackbar>
+        </form>
+      </Container>
+    </div>
   );
 };
 
--- src/components/SecondPage/SecondPage.tsx
+++ src/components/SecondPage/SecondPage.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import axios from 'axios';
+import { Alert, AlertTitle } from '@mui/material';
 
-// Define the model/interface for a Post
 interface Post {
   userId: number;
   id: number;
@@ -12,14 +12,17 @@ interface Post {
 
 const SecondPage: React.FC = () => {
   const navigate = useNavigate();
+  const location = useLocation();
   const userDetails = localStorage.getItem('userDetails');
   const [posts, setPosts] = useState<Post[]>([]);
+  const [showSuccess, setShowSuccess] = useState(false);
+  const successParam = new URLSearchParams(location.search).get('success');
 
   useEffect(() => {
     if (!userDetails) {
       alert('Please provide your details before accessing this page');
       navigate('/');
-      return; // Exit early if userDetails are missing
+      return;
     }
 
     async function fetchPosts() {
@@ -32,10 +35,25 @@ const SecondPage: React.FC = () => {
     }
 
     fetchPosts();
-  }, [navigate, userDetails]);
+
+    if (successParam) {
+      setShowSuccess(true);
+      const timer = setTimeout(() => {
+        setShowSuccess(false);
+        navigate('/second');
+      }, 3000); // 3 seconds
+      return () => clearTimeout(timer);
+    }
+  }, [navigate, userDetails, successParam]);
 
   return (
     <div>
+      {showSuccess && (
+        <Alert severity="success" onClose={() => navigate('/second')}>
+          <AlertTitle>Success</AlertTitle>
+          Form submitted successfully!
+        </Alert>
+      )}
       <h2>Second Page</h2>
       <p>Welcome to the second page!</p>
       <ul>
